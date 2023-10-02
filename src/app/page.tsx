4c1061c7bc6083402f40e8077483897735c25f8e
jmphilips <jmichaelphilips@gmail.com>
2023-10-02 14:51:59 -0400

    Install and run prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import KitchenComponent from "@/app/kitchen-component";
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-            <h1>Welcome to the Cupcake factory</h1>
-          <KitchenComponent/>
-        </div>
-      </main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Welcome to the Cupcake factory</h1>
+        <KitchenComponent />
+      </div>
+    </main>
+  );
 }
